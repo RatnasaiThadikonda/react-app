0dbb63fba1755326eaa0cc1fd8d994d98c063cb8
Ratnasai <ratnasai.t@digitelenetworks.com>
2023-12-30 23:57:48 +0530

    signup

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
 // App.js
 
-import React , {useEffect, useState} from 'react';
-import './App.css'; // Import your styles if needed
-import Display from './display'; // Import other components as needed
+import React, { useState } from "react";
+import "./App.css"; // Import your styles if needed
+
 
 function App() {
-  // const name = "Ratna";
-  const [name , setName] = useState(1);
-  const [val , setVal] = useState("");
-  useEffect(()=> console.log(name+1), [name]);
-  const handler = e=>{
-    setVal(e.target.value)
+
+  const [data, setData] = useState({
+    username: "",
+    email:"",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const changeHandler =e =>{
+    setData({...data, [e.target.name]:e.target.value})
+  }
+  const submitHandler = e =>{
+    e.preventDefault()
+    console.log(data )
   }
 
+  const { username, password , email , confirmPassword } = data;
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className='red'>Hello {name}</h1>
-        <center>
-        <Display name={name} />
-        <button onClick = {()=> setName(name+1) }>
-          ClickMe
-        </button>
-        <input value={val} onChange={(event) => [console.log(event), setVal(event.target.value)]} type="text" />
+    <div >
+      <center>
+        <form autoComplete="off" onSubmit={submitHandler}>
+          <input  value={username} name="username" type="text" onChange={changeHandler} /> <br/>
+          <input  value={email} name="email" type="email" onChange={changeHandler} /> <br/>
+          <input value={password} name="password" type="password" onChange={changeHandler} /> <br/>
+          <input value={confirmPassword} name="confirmPassword" type="password" onChange={changeHandler} /> <br/>
+          <input type="submit" name="submit" />
+        </form>
+      </center>
 
-
-        <p>{val}</p>
-
-        </center>
-      
-
-      </header>
     </div>
   );
 }
