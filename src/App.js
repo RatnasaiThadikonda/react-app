5062837f0eb11c514b5e0c57b7beacab9ec4184d
Ratnasai <ratnasai.t@digitelenetworks.com>
2023-12-31 00:16:07 +0530

    calculator

--- src/App.js
+++ src/App.js
@@ -6,32 +6,53 @@ import "./App.css"; // Import your styles if needed
 
 function App() {
 
-  const [data, setData] = useState({
-    username: "",
-    email:"",
-    password: "",
-    confirmPassword: "",
-  });
+
+  const [input , setInput] = useState('');
+  const [result , setResult] = useState(0);
+
 
   const changeHandler =e =>{
-    setData({...data, [e.target.name]:e.target.value})
+    setInput(e.target.value)
   }
-  const submitHandler = e =>{
-    e.preventDefault()
-    console.log(data )
-  }
+  // const submitHandler = e =>{
+  //   e.preventDefault()
+  //   // console.log(data )
+  // }
 
-  const { username, password , email , confirmPassword } = data;
   return (
     <div >
       <center>
-        <form autoComplete="off" onSubmit={submitHandler}>
-          <input  value={username} name="username" type="text" onChange={changeHandler} /> <br/>
-          <input  value={email} name="email" type="email" onChange={changeHandler} /> <br/>
-          <input value={password} name="password" type="password" onChange={changeHandler} /> <br/>
-          <input value={confirmPassword} name="confirmPassword" type="password" onChange={changeHandler} /> <br/>
-          <input type="submit" name="submit" />
-        </form>
+          <input  value={input} name="input" type="text" onChange={changeHandler} /> <br/>
+
+        <button onClick={()=> setResult(eval(input)) } >Result</button>
+
+        <h4>{result}</h4>
+
+        <button onClick = {()=> setInput(input+'1')}>1</button>
+        <button onClick = {()=> setInput(input+'2')}>2</button>
+        <button onClick = {()=> setInput(input+'3')}>3</button>
+        <button onClick = {()=> setInput(input+'4')}>4</button>
+        <button onClick = {()=> setInput(input+'5')}>5</button> <br/>
+        <button onClick = {()=> setInput(input+'6')}>6</button>
+        <button onClick = {()=> setInput(input+'7')}>7</button>
+        <button onClick = {()=> setInput(input+'8')}>8</button>
+        <button onClick = {()=> setInput(input+'9')}>9</button>
+        <button onClick = {()=> setInput(input+'9')}>0</button> <br/>
+
+
+        <button onClick = {()=> setInput(input+'+')}>+</button>
+
+        <button onClick = {()=> setInput(input+'-')}>-</button>
+
+        <button onClick = {()=> setInput(input+'*')}>*</button>
+
+        <button onClick = {()=> setInput(input+'/')}>/</button>
+
+        <button onClick = {()=> setInput('')}>clr</button>
+
+         
+
+     
       </center>
 
     </div>
